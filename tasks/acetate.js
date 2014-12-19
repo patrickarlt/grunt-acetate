e2e4d6872b06d16ec0ba1778344f970855fccb5f
Patrick Arlt <patrick.arlt@gmail.com>
2014-12-19 12:51:45 -0800

    reload on config file change

--- tasks/acetate.js
+++ tasks/acetate.js
@@ -11,49 +11,79 @@
 var Acetate = require('acetate');
 var path = require('path');
 var _ = require('lodash');
+var gaze = require('gaze');
 
 module.exports = function(grunt) {
 
   grunt.registerMultiTask('acetate', 'Grunt plugin for the Acetate static site builter.', function() {
     var done = this.async();
-    var data = this.data;
+    var data = this.data || grunt.config('acetate');
+    var configPath = path.join(process.cwd(), data.config);
     var options = this.options({
       keepalive: false,
       server: false,
       watch: false,
-      port: 3000
+      port: 3000,
+      host: 'localhost',
+      findPort: true,
+      logLevel: 'verbose'
     });
 
-    console.log(options);
+    var acetate;
 
     function build() {
       acetate.build(function(){
-        console.log(options.keepalive);
         if(!options.keepalive) {
           done();
         }
       });
     }
 
-    var acetate = new Acetate(path.join(process.cwd(), data.config));
+    function run() {
+      acetate = new Acetate(configPath);
 
-    _.defaults(acetate.args, data.args);
-    _.defaults(acetate.options, options);
+      _.defaults(acetate.args, data.args);
+      _.defaults(acetate.options, options);
 
-    if(options.watch){
-      acetate.watcher.start();
-    }
-
-    if(options.server && options.port){
-      acetate.server.start(options.port);
-    }
-
-    acetate.load(function(){
-      if(options.clean){
-        acetate.clean(build);
-      } else {
-        build()
+      if(options.watch){
+        acetate.watcher.start();
       }
-    });
+
+      if(options.server && options.port){
+        acetate.server.start(options.host, options.port, options.findPort);
+      }
+
+      acetate.load(function(){
+        if(options.clean){
+          acetate.clean(build);
+        } else {
+          build();
+        }
+      });
+    }
+    if(options.watch){
+      gaze(data.config, function(error, watcher){
+
+        // whenever it chagnes
+        watcher.on('changed', function() {
+
+          // stop the server if we were running it
+          if(options.server && options.port){
+            acetate.server.stop();
+          }
+
+          // stop the watcher if we were running it
+          if(options.watch){
+            acetate.watcher.stop();
+          }
+
+          // rerun everything
+          run();
+
+        });
+      });
+    }
+
+    run();
   });
 };
